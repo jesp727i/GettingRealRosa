b42700db24e4fd5d3754f7ac46f26eb77aad544c
jespe <jespe@DESKTOP-RE9P93D>
2016-11-02 11:54:46 +0100

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GettingRealRosa
+{
+    [TestClass]
+    public class Functions
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
