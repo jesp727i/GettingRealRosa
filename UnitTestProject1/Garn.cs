116509e9fd40ba8129c26d0a9c904a4fb9294b5c
jesper <jespe@DESKTOP-RE9P93D>
2016-11-03 08:41:30 +0100

    update 1.0

---
+++ UnitTestProject1/Garn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GettingRealRosa
+{
+    internal class Garn
+    {
+        public string Name;
+        public string Farve;
+        public string Type;
+        public int Amount;
+        public float Price;
+
+        internal void AddAmount(int amount)
+        {
+            Amount += amount;
+        }
+
+        internal void SubAmount(int amount)
+        {
+            Amount -= amount;
+        }
+    }
+
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -4,12 +4,68 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GettingRealRosa
 {
     [TestClass]
-    public class Functions
+    public class Opretgarn
     {
+
+        Garn garn = new Garn();
+        
         [TestMethod]
-        public void TestMethod1()
+        public void OpretGarnNavn()
+        {
+            
+            garn.Name = "Alpaka";
+            string result = garn.Name;
+            Assert.AreEqual("Alpaka", result);
+        }
+        [TestMethod]
+        public void OpretGarnFarve()
+        {
+            garn.Farve = "Rød";
+            string result = garn.Farve;
+            Assert.AreEqual("Rød", result);
+        }
+        [TestMethod]
+        public void OpretGarnType()
+        {
+            garn.Type = "Alpaka";
+            string result = garn.Type;
+            Assert.AreEqual("Alpaka", result);
+        }
+        [TestMethod]
+        public void OpretGarnMaengde()
+        {
+            garn.Amount = 1;
+            int result = garn.Amount;
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void OpretGarnPris()
+        {
+            garn.Price = 192.10f;
+            float result = garn.Price;
+            Assert.AreEqual(192.10f, result);
+        }
+        [TestMethod]
+        public void laegTilMaengde()
+        {
+            garn.Amount = 1;
+            garn.AddAmount(9);
+            int result = garn.Amount;
+            Assert.AreEqual(10,result);
+        }
+        [TestMethod]
+        public void TraekFraMaengde()
+        {
+            garn.Amount = 10;
+            garn.SubAmount(9);
+            int result = garn.Amount;
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void a()
         {
             Assert.AreEqual(1,1);
         }
+
     }
 }
